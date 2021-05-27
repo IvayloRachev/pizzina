dd5a38ef25c4921402e21abb74b6715fd1ca5f9b
IvayloRachev <82808251+IvayloRachev@users.noreply.github.com>
2021-05-27 16:44:35 +0200

    Add files via upload

---
+++ pizzina/script.js
@@ -0,0 +1,46 @@
+//menu burger
+function showResponsiveMenu() {
+    var menu = document.getElementById("topnav_responsive_menu");
+    var icon = document.getElementById("topnav_hamburger_icon");
+    var root = document.getElementById("root");
+    if (menu.className === "") {
+      menu.className = "open";
+      icon.className = "open";
+      root.style.overflowY = "hidden";
+    } else {
+      menu.className = "";                    
+      icon.className = "";
+      root.style.overflowY = "";
+    }
+  }
+//fin de menu burger
+
+//slider
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+//fin de slider
+
---
+++ pizzina/script2.js
@@ -0,0 +1,17 @@
+//modal
+var modal = document.getElementById("myModal");
+var i;
+var img = document.getElementsByClassName("myImg");
+var modalImg = document.getElementById("img01");
+ for(i=0;i< img.length;i++)
+   {    
+    img[i].onclick = function(){
+    modal.style.display = "block";
+       modalImg.src = this.src;
+ }
+}
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() { 
+   modal.style.display = "none";
+}
+//fin de modal
